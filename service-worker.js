9c4aca57924aff0b73f6ccf2a602b5b01fa323ca
loroverde-code <loroverdemc@gmail.com>
2026-02-03 17:44:13 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,5 @@
-const CACHE = "ventas-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./auth.js",
-  "./storage.js",
-  "./reports.js"
-];
-
-self.addEventListener("install", e => {
+self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.open("ventas").then(c=>c.addAll(["./"]))
   );
 });
