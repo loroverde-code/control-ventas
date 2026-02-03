47ff3be4b866c6096b8ce1e87037e1e23beb7258
loroverde-code <loroverdemc@gmail.com>
2026-02-03 17:34:48 -0600

    Update reports.js

--- reports.js
+++ reports.js
@@ -1,16 +1,19 @@
-function ventasPorFecha(fecha) {
-  return getVentas().filter(v => v.fecha === fecha);
-}
-
-function resumenDia(fecha) {
-  let ventas = ventasPorFecha(fecha);
-  let total = 0;
-  let ganancia = 0;
-
-  ventas.forEach(v => {
+function reporteDia(fecha){
+  let ventas = getVentas().filter(v=>v.fecha===fecha);
+  let total=0, ganancia=0;
+  ventas.forEach(v=>{
     total += Number(v.total);
     ganancia += Number(v.ganancia);
   });
+  return {ventas,total,ganancia};
+}
 
-  return { ventas, total, ganancia };
+function reporteMes(mes){
+  let ventas = getVentas().filter(v=>v.fecha.startsWith(mes));
+  let total=0, ganancia=0;
+  ventas.forEach(v=>{
+    total += Number(v.total);
+    ganancia += Number(v.ganancia);
+  });
+  return {total,ganancia};
 }
